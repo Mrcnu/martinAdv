1f8516e825e3f3f0dfa024b78be37d04e756d5c1
Mr.Rolex <139060034+yogi444@users.noreply.github.com>
2023-10-07 23:14:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ then
   git clone https://github.com/yogi444/MARTIN-BOT-2.git /MARTIN-BOT-2
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Auto-search-tamil-bot
+  git clone $UPSTREAM_REPO /MARTIN-BOT-2
 fi
-cd /Auto-search-tamil-bot
+cd /MARTIN-BOT-2
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
