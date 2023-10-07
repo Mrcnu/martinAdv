724cf60e62cbf71b3b64868d70de09d6de8f245b
Mr.Rolex <139060034+yogi444@users.noreply.github.com>
2023-10-07 21:01:40 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,1 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/cobra1294/MARTIN-BOT-2.git /MARTIN-BOT-2 
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /MARTIN-BOT-2 
-fi
-cd /MARTIN-BOT-2 
-pip3 install -U -r requirements.txt
-echo "Starting Bot...."
-python3 bot.py
+
