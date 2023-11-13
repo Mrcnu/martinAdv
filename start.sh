965bdaa37bc2935bd7157ce10c0f4f8e6e6d8925
Mrcnu <134508260+Mrcnu@users.noreply.github.com>
2023-11-13 13:37:15 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z "$UPSTREAM_REPO" ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/yogi444/MARTIN-BOT.git /MARTIN-BOT
+  git clone https://github.com/Mrcnu/martinAdv.git /martinAdv
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /MARTIN-BOT
+  git clone $UPSTREAM_REPO /martinAdv
 fi
-cd /MARTIN-BOT
+cd /martinAdv
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
